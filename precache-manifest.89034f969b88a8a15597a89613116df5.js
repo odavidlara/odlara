e4acee65330747e493443798b9ae887529f94eea
davidlara <oscar.lr97@gmail.com>
2020-04-18 23:34:47 -0700

    Updates

---
+++ precache-manifest.89034f969b88a8a15597a89613116df5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a62e506e86885bc27d7e10c556093ad6",
+    "url": "/odlara/index.html"
+  },
+  {
+    "revision": "35851bb7b513d8de8589",
+    "url": "/odlara/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a4c5cd557bd91208ab1a",
+    "url": "/odlara/static/js/2.84bfeb7c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/odlara/static/js/2.84bfeb7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35851bb7b513d8de8589",
+    "url": "/odlara/static/js/main.aa68ae07.chunk.js"
+  },
+  {
+    "revision": "e2a1ba447f4f56e778c5",
+    "url": "/odlara/static/js/runtime-main.1f11e86a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/odlara/static/media/logo.5d5d9eef.svg"
+  }
+]);
